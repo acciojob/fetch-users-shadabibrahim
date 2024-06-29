abc54bed7f776beac3866aa36174e513e0a6d7aa
Mohd Shadab <shadab_ibrahim@outlook.com>
2024-06-29 17:57:56 +0530

    submitting code-Sat Jun 29 2024 17:57:55 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,43 @@
-
-import React from "react";
+import 'regenerator-runtime/runtime';
+import React, { useState, useEffect } from "react";
 import './../styles/App.css';
+import Axios from 'axios';
+import DispalyUserData from './Display';
+// import { Table } from 'antd';
+// import { fetchUser } from "./fetchUser";
 
 const App = () => {
+
+
+  const [userInfo, setuserInfo] = useState([]);
+  const [disp, setDisp] = useState(false);
+  const getUser = async () => {
+    try {
+      const response = await Axios({
+        method: 'GET',
+        url: 'https://reqres.in/api/users'
+      })
+      // console.log(response.data.data)
+      setuserInfo(response.data.data)
+
+    } catch (error) {
+      alert(error.message)
+    }
+
+  }
+  useEffect(() => {
+    getUser()
+  }, [])
+
+  const handleClick = () => {
+    setDisp(true)
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <button className=".btn" onClick={handleClick} >Get User List</button>
+      {disp ? <DispalyUserData userInfo={userInfo} /> : <span><h5>No data fount to be display.</h5></span>}
+
     </div>
   )
 }
---
+++ src/components/Display.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./style.css"
+function DispalyUserData({ userInfo }) {
+    return (
+        <div>
+            <table>
+                <thead>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Avatar</th>
+                </thead>
+                <tbody>
+                {userInfo.map((item) => {
+                    return (
+                        <tr key={item.id}>
+                            <td>{item.first_name}</td>
+                            <td>{item.last_name}</td>
+                            <td>{item.email}</td>
+                            <td><img src={item.avatar} alt="" /> </td>
+                        </tr>
+                    )
+                })}
+                </tbody>
+            </table>
+        </div>
+    )
+}
+export default DispalyUserData;
