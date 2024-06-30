508dce6c6493da724defc6835916453bdb9a6ad5
Mohd Shadab <shadab_ibrahim@outlook.com>
2024-06-30 20:55:29 +0530

    submitting code-Sun Jun 30 2024 20:55:29 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,58 +1,17 @@
+// src/App.js
+import React from 'react';
+import './App.css';
+import UserList from './UserList'
 
-import React, {useState} from "react";
-import './../styles/App.css';
-
-const App = () => {
-  const [data, setData] = useState(null);
-
-  const fetchUsers = async () => {
-    try {
-      const response = await fetch("https://reqres.in/api/users");
-      const data = await response.json();
-      setData(data.data);
-    } catch (error) {}
-  };
-
+function App() {
   return (
-    <div>
-      <div className="header">
-        <h2>Blue Whales App</h2>
-        <button className="btn" onClick={fetchUsers}>
-          Get User List
-        </button>
-      </div>
-      <table>
-        <thead>
-          <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Email</th>
-            <th>Avatar</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data ? (
-            data.map((user) => {
-              return (
-                <tr key={user.id}>
-                  <td>{user.first_name}</td>
-                  <td>{user.last_name}</td>
-                  <td>{user.email}</td>
-                  <td>
-                    <img src={user.avatar} />
-                  </td>
-                </tr>
-              );
-            })
-          ) : (
-            <tr>
-              <td>No data found</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+    <div className="App">
+      <header className="App-header">
+        <h1>Blue Whales App</h1>
+        <UserList />
+      </header>
     </div>
   );
-};
+}
 
 export default App;
